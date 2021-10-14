73d6a0d75ae6629bc3f192d43514170da844a4a9
HarisSaran <hsaran100@academic.rrc.ca>
2021-10-14 15:48:47 -0500

    displaying games acosiated with their publisher and number of games sold through index.html

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
+  # this goes to views > games > index
   def index
+    @games = Game.includes(:publisher_company).all
   end
 
+  # this goes to views > games > show
   def show
+    @game = Game.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
-  get 'publisher_companies/index'
-  get 'publisher_companies/show'
+  # get 'games/index'
+  # get 'games/show'
+  # get 'publisher_companies/index'
+  # get 'publisher_companies/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # to populate the games index with info go to games controler INDEX!
+  resources :games, only: %i[index show]
+
+  resources :publisher_companies, only: %i[index show]
 end
