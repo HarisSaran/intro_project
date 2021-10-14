a16eb49199d0299cf410dfd2b4f65d4461e2b73e
HarisSaran <hsaran100@academic.rrc.ca>
2021-10-14 14:22:30 -0500

    created seeds and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+Game.delete_all
+PublisherCompany.delete_all
+
+filename = Rails.root.join("db/GamesSales.csv")
+
+puts "loading games from CSV: #{filename}"
+
+csv_data = File.read(filename)
+games = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+games.each do |m|
+  puts m["Game"]
+end
