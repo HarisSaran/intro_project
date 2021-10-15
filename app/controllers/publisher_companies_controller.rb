f2d9c9e11b712c378ba2658c9c8a5fa816410501
HarisSaran <hsaran100@academic.rrc.ca>
2021-10-15 15:53:49 -0500

    added Bulma to application html

--- app/controllers/publisher_companies_controller.rb
+++ app/controllers/publisher_companies_controller.rb
@@ -1,7 +1,9 @@
 class PublisherCompaniesController < ApplicationController
   def index
+    # @publisher_companies = PublisherCompany.all
   end
 
   def show
+    # @publisher_company = PublisherCompany.find(params[:id])
   end
 end
