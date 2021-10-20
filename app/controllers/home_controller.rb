a97b02081308e64b456f89110906e26104f6da1d
HarisSaran <hsaran100@academic.rrc.ca>
2021-10-20 10:18:35 -0500

    created publisher companies statement to display the top publishing companies based on how many games they created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
     @games = Game.includes(:publisher_company)
                  .order("north_america_sales DESC")
                  .limit(15)
-    # @publisher_companies =
+    @publisher_companies = PublisherCompany.select("publisher_companies.*")
+                                           .select("COUNT(publisher_companies.id as game_count")
+                                           .left_joins(:games)
+                                           .group("publisher_companies.id")
+                                           .order("game_count DESC")
   end
 end
