9658b0e9f63a054b9c994ecedf3183bbb58e052b
HarisSaran <hsaran100@academic.rrc.ca>
2021-10-21 10:45:51 -0500

    redirected pages controller with permalink path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,8 +26,8 @@ class PagesController < ApplicationController
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to @page, notice: "Page was successfully created." }
-        format.json { render :show, status: :created, location: @page }
+        format.html { redirect_to permalink_path(@page.permalink), notice: "Page created." }
+        format.json { render :show, status: :created, location: permalink_path(@page.permalink) }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @page.errors, status: :unprocessable_entity }
@@ -39,8 +39,8 @@ class PagesController < ApplicationController
   def update
     respond_to do |format|
       if @page.update(page_params)
-        format.html { redirect_to @page, notice: "Page was successfully updated." }
-        format.json { render :show, status: :ok, location: @page }
+        format.html { redirect_to permalink_path(@page.permalink), notice: "Page updated." }
+        format.json { render :show, status: :ok, location: permalink_path(@page.permalink) }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @page.errors, status: :unprocessable_entity }
